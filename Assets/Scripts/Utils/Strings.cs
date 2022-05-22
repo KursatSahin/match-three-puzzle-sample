87632d83ecd19db9c40bf0a9c6e3cd4f9b387768
Kursad Sahin <kursad.sahin@mynet.games>
2022-05-22 15:18:09 +0300

    Project was initialized with most used libs and frameworks

---
+++ Assets/Scripts/Utils/Strings.cs
@@ -0,0 +1,22 @@
+﻿namespace Utils
+{
+    public static class Strings
+    {
+        public static class Home
+        {
+            public const string HomePlayButtonText = "PLAY";
+            public const string HomeExitButtonText = "EXIT";
+        }
+        
+        public static class Common
+        {
+            public const string LoadingText = "Loading!";
+        }
+        
+        public static class InGameUI
+        {
+            public const string RestartButtonText = "RESTART";
+            public const string ExitButtonText = "EXIT";
+        }
+    }
+}
